c6dd5d8a88a046ad9fa20bd4067864e04a295cdd
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-03 13:12:48 -0800

    adds s to http for API key to resolve google block

--- src/services/dragRaceApi.js
+++ src/services/dragRaceApi.js
@@ -1,9 +1,9 @@
 export const getQueens = () => {
-  return fetch('http://www.nokeynoshade.party/api/queens?limit=50')
+  return fetch('https://www.nokeynoshade.party/api/queens?limit=50')
     .then(res => res.json());
 };
 
 export const getOneQueen = (id) => {
-  return fetch(`http://www.nokeynoshade.party/api/queens/${id}`)
+  return fetch(`https://www.nokeynoshade.party/api/queens/${id}`)
     .then(res => res.json());
 };
