0a62063b09bb4e54f4bf204858fa2731f775c7f8
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-02-02 19:23:31 -0800

    removes dead code from Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,10 @@
-
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
 export default class Header extends React.Component {
   render() {
     return (
       <>
         <h1>RuPaul's Drag Race</h1>
-        <ul>
-          {/* <li>
-            <Link to="/">Home</Link>
-          </li> */}
-          {/* <li>
-            <Link to="/list" >List</Link>
-          </li> */}
-        </ul>
       </>
     )
   }
